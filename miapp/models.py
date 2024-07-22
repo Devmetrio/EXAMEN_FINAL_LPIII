b486d82c4227f2ff7f45eed99f1a7ecd42aa3dc7
german <briph21@gmail.com>
2024-07-22 15:12:57 -0500

    AMES Examen Final - Parte 2 - Base de Datos

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class AMES_Persona(models.Model):
+    select_sexo = (
+        ('Hombre', 'Hombre'),
+        ('Mujer', 'Mujer')
+    )
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    sexo = models.CharField(max_length=8, choices=select_sexo)
+    fecha_de_registro = models.DateTimeField(auto_now_add=True)
